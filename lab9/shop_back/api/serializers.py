a6f09f74ae2ee80d02a066dd58158ea0441d4994
Rustem_Murzagaliyev <r_murzagaliyev@kbtu.kz>
2026-03-31 13:30:01 +0500

    lab9
    lab9

---
+++ lab9/shop_back/api/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from .models import Categories, Product
+
+class CategoriesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Categories
+        fields ='__all__'
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields ='__all__'
